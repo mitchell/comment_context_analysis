bc697d4909767a59d541dce155188657771c52c6
mitchelljfs <mitchelljfsimon@gmail.com>
2017-11-28 23:09:03 -0800

    created ruby script setting up neo4j database and schema from json objects also uploaded

---
+++ json_to_neo4j.rb
@@ -0,0 +1,83 @@
+require 'json'
+require 'neo4j-core'
+require 'neo4j/core/cypher_session/adaptors/http'
+require 'ruby-progressbar'
+
+# sreate adaptor and session to Neo4j server
+adaptor = Neo4j::Core::CypherSession::Adaptors::HTTP.new 'http://neo4j:polarbear@localhost:7474' # rubocop:disable Metrics/LineLength
+session = Neo4j::Core::CypherSession.new adaptor
+
+# read contents of file to file
+file = File.read './first10000.json'
+
+# set up variables to read file line-by-line into an array of hashes
+file_array = []
+counter = 0
+file.each_line do |line|
+  line_hash = JSON.parse line
+  file_array[counter] = line_hash
+  counter += 1
+end
+
+# unwind and load array of hashes hash-by-hash into query and run query
+query_progress = ProgressBar.create(title: 'Load Queries', starting_at: 0,
+                                    total: file_array.length)
+to_query = <<QUERY
+  UNWIND {json} as properties
+  CREATE (c:Comment) SET c = properties
+QUERY
+file_array.each do |data|
+  session.query to_query, json: data
+  query_progress.increment
+end
+# session.query 'CREATE INDEX ON :Comment(id)'
+
+other_progress = ProgressBar.create(title: 'Other Queries', starting_at: 0,
+                                    total: 5)
+# create Subreddits label and nodes
+to_query = <<QUERY
+  MATCH (c:Comment)
+  MERGE (s:Subreddit {id: c.subreddit_id})
+    ON CREATE SET s.name = c.subreddit
+    ON MATCH SET s.comment_count = coalesce(s.comment_count, 0) + 1
+QUERY
+session.query to_query
+# session.query 'CREATE INDEX ON :Subreddit(name)'
+other_progress.increment
+
+# create relationship between posts with subreddits that match
+to_query = <<QUERY
+  MATCH (c:Comment)
+  WITH c
+  MATCH (s:Subreddit)
+  WHERE c.subreddit_id = s.id
+  CREATE (c)-[:POSTED_IN]->(s)
+QUERY
+session.query to_query
+other_progress.increment
+# remove overlapping properties with subreddits from comments
+to_query = <<QUERY
+  MATCH (c:Comment)
+  REMOVE c.subreddit_id, c.subreddit
+QUERY
+session.query to_query
+other_progress.increment
+
+# create Author label, nodes, and relationships to comments
+to_query = <<QUERY
+  MATCH (c:Comment)
+  MERGE (a:Author {name: c.author})
+    ON MATCH SET a.comment_count = coalesce(a.comment_count, 0) + 1
+  CREATE (a)-[:WROTE]->(c)
+QUERY
+session.query to_query
+other_progress.increment
+# session.query 'CREATE INDEX ON :Author(name)'
+
+# remove overlapping properties with comments from authors
+to_query = <<QUERY
+  MATCH (c:Comment)
+  REMOVE c.author
+QUERY
+session.query to_query
+other_progress.increment
